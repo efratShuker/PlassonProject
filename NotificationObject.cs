47598a81da42edfacd1adfbb385c189db16d3cec
efratShuker <86189441+efratShuker@users.noreply.github.com>
2024-03-05 12:04:54 +0200

    Delete Unnecessary code

--- NotificationObject.cs
+++ NotificationObject.cs
@@ -6,12 +6,6 @@ namespace MachineStatusTracker
     public class NotificationObject : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
-
-        //protected void SetProperty<T>(ref T reference, T value, string propertyName)
-        //{
-        //    if (PropertyChanged != null)
-        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        //}
         protected void OnNotifyPropertyChanged(string propertyName)
         {
             if (PropertyChanged != null)
